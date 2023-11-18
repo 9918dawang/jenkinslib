d5734d10c4b491332a9988979bcdd1e9309b7466
9918dawang <61612678+9918dawang@users.noreply.github.com>
2023-11-19 00:06:26 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,5 +1,17 @@
 package org.devops
-
+//打印
 def PrintMe(content){
   printf(content)
+}
+
+
+//格式化输出
+def PrintMes(value,color){
+   colors=['red':"\033[40;31n >>>>>>>>>>>${value}<<<<<<<<< \033[0n",
+          'blue':"\033[47;34n ${value} \033[0n",
+           'green':".[1;32m >>>>>>>>>>>${value}<<<<<<<<< [n",
+           'green1':"\033[40;32m >>>>>>>>>>>${value}<<<<<<<<< \033[0m"]
+  ansiColor('xterm') {
+    println(colors[color])
+  }
 }
